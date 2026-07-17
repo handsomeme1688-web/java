6ba96fb9ef6d681f8329de4db2a543fab559550f
zoee <hamburgeryummy614@gmail.com>
2026-07-17 21:12:19 +0800

    day3补手写sql、yaml里加入mp的log打印

--- day3/src/main/java/com/zoee/day3/mapper/UserMapper.java
+++ day3/src/main/java/com/zoee/day3/mapper/UserMapper.java
@@ -3,7 +3,10 @@ package com.zoee.day3.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zoee.day3.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
+    @Select("select * from user where id=#{id}")
+    User findById(Long id);
 }
--- day3/src/test/java/com/zoee/day3/UserServiceTest.java
+++ day3/src/test/java/com/zoee/day3/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.zoee.day3;
 
 
 import com.zoee.day3.entity.User;
+import com.zoee.day3.mapper.UserMapper;
 import com.zoee.day3.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,8 @@ public class UserServiceTest {
 
     @Autowired
     UserService userService;
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     void getById(){
@@ -25,9 +28,18 @@ public class UserServiceTest {
     }
 
     @Test
-    void getById_不存在的id_返回null() {
+    void findById(){
+        User u2 = userMapper.findById(1L);
+        assertEquals("user02",u2.getUsername());
+        assertEquals("张伟",u2.getNickname());
+    }
+
+    @Test
+    void getById_null() {
         assertNull(userService.getById(999L));
     }
+
+
 
 
 
